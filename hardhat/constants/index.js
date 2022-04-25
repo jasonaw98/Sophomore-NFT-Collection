7eddb3c68a0a936912d21d74aee0254e2fc2ce18
Jasonaw <jason.aw@grassvalley.com>
2022-04-25 17:10:58 +0800

    stage

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xC9591e6790fF67F933BA297482c48ed2889E6A11";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://whitelist-dapp-phi-rosy.vercel.app/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
